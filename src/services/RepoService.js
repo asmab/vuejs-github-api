f535b7fb130b5809c567f7722becad16100c21f2
Circle_User <asma.boussabat.asma@gmail.com>
2018-08-15 10:40:23 +0200

    remove authentication fron api calls

--- src/services/RepoService.js
+++ src/services/RepoService.js
@@ -5,10 +5,10 @@ const accessToken = '4951cdd1649d7a05a97c35116e334f6e99bce013'
 export default {
 
     getRepositories() {
-        return Api().get('/orgs/futurice/repos'+'?access_token='+accessToken)
+        return Api().get('/orgs/futurice/repos')
     },
-    getContributors(repo_name){
-        console.log('/repos/'+ repo_name + '/contributors'+'?access_token='+accessToken)
-        return Api().get('/repos/'+ repo_name + '/contributors'+'?access_token='+accessToken)
-    } 
+    getContributors(repo_name) {
+        //with authentication : +'?access_token='+accessToken
+        return Api().get('/repos/' + repo_name + '/contributors')
+    }
 }
